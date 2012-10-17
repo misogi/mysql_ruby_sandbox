c60fbc5cec7080d314cb4250a1b14501c218d89f
misogi <ryudo@misogi.info>
2012-10-17 15:18:44 +0900

    add model and connect db

--- start.rb
+++ start.rb
@@ -1,5 +1,19 @@
 require 'rubygems'
 require 'sequel'
+Sequel::Model.plugin(:schema)
 
-DB = Sequel.mysql2('dbname')
+DB = Sequel.mysql2('sandbox', password: '1234', user: 'root')
 
+class Hero < Sequel::Model
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      integer :power
+      integer :dex
+      integer :intelligence
+      timestamp :create_date
+    end
+    create_table
+    DB.add_index :heros, :dex
+  end
+end
