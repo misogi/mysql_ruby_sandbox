ebaf4208c8769d7994a9452955dca969a9f810e5
misogi <ryudo@misogi.info>
2012-10-17 15:40:46 +0900

    gen 100000 data

--- gendata.rb
+++ gendata.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'model'
 
-100.times do
+100000.times do
   Hero.create(power: rand(20000000), dex: rand(2000000), intelligence: rand(2000000))
 end
