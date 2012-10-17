289f7789a545c8c4fb51ac3e78f9c34a0f4c08a9
misogi <ryudo@misogi.info>
2012-10-17 15:53:34 +0900

    add Follower genarate

--- gendata.rb
+++ gendata.rb
@@ -1,6 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'model'
+require 'lorem'
 
-100000.times do
+0.times do
   Hero.create(power: rand(20000000), dex: rand(2000000), intelligence: rand(2000000))
+end
+
+
+0.times do
+  id = rand(100099)
+  hero = Hero.find(id: id)
+  follower = Follower.create(name: Lorem::Base.new('words', 10).output, create_date: Time.now)
+  follower.hero = hero
 end
